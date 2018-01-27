78f79037482d8876d800a954ae2a2ddfe68d41c8
Nick Fox-Gieg <nick@fox-gieg.com>
2018-01-27 15:55:04 -0500

    first

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rotator : MonoBehaviour {
+
+    public Vector3 rot;
+
+	private void Update() {
+        transform.Rotate(rot);
+	}
+
+}
